0f11919c371304560757038229a97ce5941a6488
Akmal <akmalthemalong@gmail.com>
2025-09-04 20:37:37 +0800

    chore: ubah nama aplikasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>{{ $title ?? config('app.name') }}</title>
+  <title>{{ $title ?? 'SiLele' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
